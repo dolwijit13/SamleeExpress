99253315f271c749a95cf4bc471cb3e66a76960b
Watcharit Tuntayakul <watcharit.new@gmail.com>
2019-11-18 17:15:23 +0700

    initial react

--- samlee-frontend/src/App.js
+++ samlee-frontend/src/App.js
@@ -3,16 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {apiResponse: ""};
+  }
+
+  callAPI(){
+    alert("start");
+    fetch("http://localhost:3001").then(res=>res.text).then(res=>this.setState({apiResponse: res})).catch(err => err);
+    alert("fin");
+  }
+
+  componentWillMount(){
+    this.callAPI();
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <p>this is react</p>
+        <p>{this.state.apiResponse}</p>
       </div>
     );
   }
