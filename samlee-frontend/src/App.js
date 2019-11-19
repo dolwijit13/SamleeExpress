6a5ea2694bb624270960a2c01cf143aac4a234e3
Dolwijit <dol51890@gmail.com>
2019-11-19 09:23:21 +0700

    display customer info for test

--- samlee-frontend/src/App.js
+++ samlee-frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/*
 class App extends Component {
   constructor(props) {
     super(props);
@@ -27,5 +28,65 @@ class App extends Component {
     );
   }
 }
+
+export default App;
+*/
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    users: [],
+    error: null
+  };
+
+  fetchUsers() {
+    fetch(`http://localhost:3000/customer`)
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          users: data,
+          isLoading: false,
+        })
+      )
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+  render() {
+    const { isLoading, users, error } = this.state;
+    return (
+      <React.Fragment>
+
+        <form>
+          <label>
+            Name:
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+
+        <h1>Customer</h1>
+        {error ? <p>{error.message}</p> : null}
+        {!isLoading ? (
+          users.map(user => {
+            const { RegisterID, FirstName, LastName } = user;
+            return (
+              <div key={RegisterID}>
+                <p>FirstName: {FirstName}</p>
+                <p>LastName: {LastName}</p>
+                <hr />
+              </div>
+            );
+          })
+        ) : (
+          <h3>Loading...</h3>
+        )}
+      </React.Fragment>
+    );
+  }
+}
+
 
 export default App;
