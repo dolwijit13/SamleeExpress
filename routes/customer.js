3e42667061f83b583ca619b427bde5021cfe47bf
Dolwijit <dol51890@gmail.com>
2019-11-20 17:44:19 +0700

    delete pug

--- routes/customer.js
+++ routes/customer.js
@@ -15,24 +15,6 @@ router.get('/',(req,res) => {
 
 //Add
 
-<<<<<<< HEAD
-/*wait
-//Create
-router.get('/add',(req,res) => {
-		res.render('addCustomer');
-});
-
-//*
-||||||| merged common ancestors
-//*wait
-//Create
-router.get('/add',(req,res) => {
-		res.render('addCustomer');
-});
-
-//*
-=======
->>>>>>> b5733e19b35222545c91d71428bc68b5c7d8e8c5
 //RegisterID,FirstName,LastName,TelephoneNo,EMail,HouseNo,Street,SubDistrict,District,Province,Country,PostalCode,StartingDate,Gender
 
 router.post('/add',(req,res) => {
@@ -82,54 +64,5 @@ router.post('/add',(req,res) => {
 
 	})
 });
-
-
-//*wait
-//Update
-router.get('/edit/:RegisterID',(req,res) => {
-	
-	const edit_ID = req.params.RegisterID;
-	
-	connection.query('SELECT * FROM CUSTOMER WHERE RegisterID=?',[edit_ID],(err,results) => {
-		if(results){
-			res.render('edit',{
-				customer:results[0]
-			});
-		}
-	});
-});
-
-
-router.post('/edit/:RegisterID',(req,res) => {
-	//can't edit primary key
-	const {FirstName,LastName,TelephoneNo,EMail,HouseNo,Street,SubDistrict,District,Province,Country,
-		PostalCode,StartingDate,Gender } = req.body;
-
-	const oldId = req.params.RegisterID;
-	
-	connection.query('UPDATE `CUSTOMER` SET FirstName = ?, LastName = ?,TelephoneNo = ?, EMail = ?, HouseNo = ?, Street = ?\
-	, SubDistric = ?t, District = ?, Province, = ? Country = ?,PostalCode = ?, StartingDate = ?, Gender = ?  WHERE RegisterID = ?',
-	[FirstName, LastName, TelephoneNo, EMail, HouseNo, Street, SubDistrict, District, Province, Country, PostalCode, StartingDate,
-		Gender,oldId], (err, results) => {
-        if(results.changedRows === 1){
-            console.log('Updated customer id : '+ RegisterID);
-        }
-		return res.redirect('/cutsomer');
-    });
-});
-
-
-
-//Delete
-router.get('/delete/:RegisterID',(req,res) => {
-    connection.query('DELETE FROM `CUSTOMER` WHERE RegisterID = ?', [req.params.RegisterID], (err, results) => {
-        return res.redirect('/cutstomer');
-    });	
-});
-
-//*/
-
-
-
 
 module.exports = router
--- routes/shipmentStatus.js
+++ routes/shipmentStatus.js
@@ -75,52 +75,6 @@ router.post('/add/:Employee_DeliverSSN',(req,res) => {
 });
 
 
-//*wait
-//Update
-router.get('/edit/:RegisterID',(req,res) => {
-	
-	const edit_ID = req.params.RegisterID;
-	
-	connection.query('SELECT * FROM CUSTOMER WHERE RegisterID=?',[edit_ID],(err,results) => {
-		if(results){
-			res.render('edit',{
-				customer:results[0]
-			});
-		}
-	});
-});
-
-
-router.post('/edit/:RegisterID',(req,res) => {
-	//can't edit primary key
-	const {FirstName,LastName,TelephoneNo,EMail,HouseNo,Street,SubDistrict,District,Province,Country,
-		PostalCode,StartingDate,Gender } = req.body;
-
-	const oldId = req.params.RegisterID;
-	
-	connection.query('UPDATE `CUSTOMER` SET FirstName = ?, LastName = ?,TelephoneNo = ?, EMail = ?, HouseNo = ?, Street = ?\
-	, SubDistric = ?t, District = ?, Province, = ? Country = ?,PostalCode = ?, StartingDate = ?, Gender = ?  WHERE RegisterID = ?',
-	[FirstName, LastName, TelephoneNo, EMail, HouseNo, Street, SubDistrict, District, Province, Country, PostalCode, StartingDate,
-		Gender,oldId], (err, results) => {
-        if(results.changedRows === 1){
-            console.log('Updated customer id : '+ RegisterID);
-        }
-		return res.redirect('/cutsomer');
-    });
-});
-
-
-
-//Delete
-router.get('/delete/:RegisterID',(req,res) => {
-    connection.query('DELETE FROM `CUSTOMER` WHERE RegisterID = ?', [req.params.RegisterID], (err, results) => {
-        return res.redirect('/cutstomer');
-    });	
-});
-
-//*/
-
-
 
 
 module.exports = router
