fb7d05e0be275f395fec9aca6d1d6f891171d359
pewt1403 <p_ngamboontham@hotmail.com>
2019-11-20 10:10:54 +0700

    Add login handler

--- routes/customer.js
+++ routes/customer.js
@@ -15,24 +15,6 @@ router.get('/',(req,res) => {
 
 //Add
 
-<<<<<<< HEAD
-/*wait
-//Create
-router.get('/add',(req,res) => {
-		res.render('addCustomer');
-});
-
-//*
-||||||| merged common ancestors
-//*wait
-//Create
-router.get('/add',(req,res) => {
-		res.render('addCustomer');
-});
-
-//*
-=======
->>>>>>> b5733e19b35222545c91d71428bc68b5c7d8e8c5
 //RegisterID,FirstName,LastName,TelephoneNo,EMail,HouseNo,Street,SubDistrict,District,Province,Country,PostalCode,StartingDate,Gender
 
 router.post('/add',(req,res) => {
--- samlee-frontend/src/login.js
+++ samlee-frontend/src/login.js
@@ -5,31 +5,50 @@ import Customer from './Customer.js';
 class App extends React.Component {
   constructor(props){
     super(props);
-    this.State = {
-	redirect: false
-	};
+    this.state = {
+      username: '',
+      password: '',
+	    redirect: false,
+	  };
+  }
+  loginHandler = (event) => {
+    event.preventDefault();
+    let passwd = this.state.password;
+    if (passwd.trim() == "") {
+      alert("Please enter password.");
+    }
+  }
+  paramField = (event) => {
+    let uname = event.target.name;
+    let passwd = event.target.value;
+    this.setState({[uname]: passwd});
   }
   render() {
     return (
+      <form onSubmit={this.loginHandler}>
       <div>
         <ul>
           <li className="left"><a>SamleeExpress</a></li>
         </ul>
-	<div className="center">
+	      <div className="center">
           <a>Welcome to SamleeExpress</a>
         </div>
         <div className="center">
-          <input type="text" placeholder="Username" />
+          <input type="text" /*placeholder="Username"*/name='username' onChange={this.paramField}/>
         </div>
-	<div className="center">
-	  <input type="text" placeholder="Password" />
+	      <div className="center">
+	        <input type="text" /*placeholder="Password"*/name='password' onChange={this.paramField}/>
         </div>
-	<div className="center">
-          <button type="submit"
-	  onClick = { <Customer /> }>Sign in</button>
+	<     div className="center">
+          <button type="submit" onClick = { <Customer /> }>Sign in</button>
+        </div>
+        <div className="center">
+          <h4>Hello {this.state.username}</h4>
         </div>
       </div>
+      </form>
     );
+  }
 }
-}
+
 export default App;
