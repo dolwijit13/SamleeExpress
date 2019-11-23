df550d82f6ff270a52a7d28ae09cf66d810ec00b
Watcharit Tuntayakul <watcharit.new@gmail.com>
2019-11-23 16:53:36 +0700

    delete the parcel in progress

--- samlee-frontend/src/EmployeeParcel.js
+++ samlee-frontend/src/EmployeeParcel.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { confirmAlert } from 'react-confirm-alert'; 
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import Axios from 'axios';
 
 class EmployeeParcel extends React.Component {
   constructor(props){
@@ -30,6 +33,28 @@ class EmployeeParcel extends React.Component {
       .catch(error => this.setState({ error, doneLoading: false }));
   }
 
+  deleteHandler(event, parcel){
+    var url = "http://localhost:8000/parcel/delete/";
+    const data = {ParcelID : parcel.ParcelID};
+    confirmAlert({
+      title: 'Confirm to delete',
+      message: "Are you sure to delete " + parcel.ParcelID + " that sent to " + parcel.HouseNo + " "+ parcel.Street + " " + parcel.SubDistrict + " " + parcel.District + " " +
+        parcel.Province + " " + parcel.Country + " " + parcel.PostalCode,
+      buttons:[
+        {
+          label: 'Yes',
+          onClick: () => {
+            Axios.delete(url,data);
+            window.location.reload();
+          }
+        },
+        {
+          label: 'No',
+        }
+      ]
+    });
+  }
+
   render() {
     if(!this.state.doneLoading) return null;
     var dataParcel = this.state.parcels.map((parcel,index)=>
@@ -43,6 +68,7 @@ class EmployeeParcel extends React.Component {
               ()=>this.props.changeParcelToEditParcel(parcel)} 
               className="btn btn-success">Edit</button></td>
             <td><button className="btn btn-primary" onClick={()=>this.props.changeParcelToShipmentStatus(parcel)}>Status</button></td>
+            <td><button className="btn btn-danger" onClick={(e)=>this.deleteHandler(e,parcel)}>Delete</button></td>
         </tr>
     );
     return (
@@ -58,6 +84,7 @@ class EmployeeParcel extends React.Component {
                     <th className="data-width-table">Terminal</th>
                     <th className="data-width-table">Type</th>
                     <th className="data-width-table">InsuranceType</th>
+                    <th></th>
                     <th></th>
                     <th></th>
                 </tr>
